9b1e555b2cf39987ae56c9c60be44eddca1fecc2
William Barnes <barnesw91291@gmail.com>
2017-01-27 19:51:02 -0800

    added interleave algorithm

---
+++ test/interleave.spec.js
@@ -0,0 +1,9 @@
+const expect = require('expect.js');
+const interleave = require('../algos/interleave.js');
+
+
+describe('interleave', () => {
+  it('should be a function', () => {
+    expect(interleave).to.be.a('function');
+  });
+});
