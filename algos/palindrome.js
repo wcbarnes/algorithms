e695a9ff02b3c119382eb0ff109c8bc67a53f832
William Barnes <barnesw91291@gmail.com>
2017-01-27 19:30:39 -0800

    convert to array palindrome solution

--- algos/palindrome.js
+++ algos/palindrome.js
@@ -1,14 +1,20 @@
+// function palindrome(str) {
+//   str = str.replace(/[^a-zA-Z]/g, '').toLowerCase();
+//   if (!str || typeof str !== 'string') return false;
+//   let pointer1 = 0;
+//   let pointer2 = str.length - 1;
+//   while (pointer1 <= pointer2) {
+//     if (str[pointer1] !== str[pointer2]) return false;
+//     pointer1++;
+//     pointer2--;
+//   }
+//   return true;
+// }
+
 function palindrome(str) {
-  str = str.replace(/[^a-zA-Z]/g, '').toLowerCase();
   if (!str || typeof str !== 'string') return false;
-  let pointer1 = 0;
-  let pointer2 = str.length - 1;
-  while (pointer1 <= pointer2) {
-    if (str[pointer1] !== str[pointer2]) return false;
-    pointer1++;
-    pointer2--;
-  }
-  return true;
+  str = str.replace(/[^a-zA-Z]/g, '').toLowerCase();
+  return str.split('').reverse().join('') === str;
 }
 
 
