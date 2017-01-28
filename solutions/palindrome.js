c7520fb3e74d0ca3d0d54a251d5bff58a4090083
William Barnes <barnesw91291@gmail.com>
2017-01-27 23:18:23 -0800

    palindrome example for solution, question, and tests

---
+++ solutions/palindrome.js
@@ -0,0 +1,40 @@
+function palindrome(str) {
+  str = str.replace(/[^a-zA-Z]/g, '').toLowerCase();
+  if (!str || typeof str !== 'string') return false;
+  let pointer1 = 0;
+  let pointer2 = str.length - 1;
+  while (pointer1 <= pointer2) {
+    if (str[pointer1] !== str[pointer2]) return false;
+    pointer1++;
+    pointer2--;
+  }
+  return true;
+}
+
+function palindrome(str) {
+  if (!str || typeof str !== 'string') return false;
+  str = str.replace(/[^a-zA-Z]/g, '').toLowerCase();
+  return str.split('').reverse().join('') === str;
+}
+
+function palindrome(str) {
+  if (!str || typeof str !== 'string') return false;
+  str = str.replace(/[^a-zA-Z]/g, '').toLowerCase();
+  function recurse(string) {
+    if (!string.length) return true;
+    if (string[0] !== string[string.length -1]) return false;
+    return recurse(string.slice(1, string.length - 1));
+  }
+  return recurse(str);
+}
+
+function palindrome(str, start = 0, end = str.length - 1) {
+  if (start === 0) {
+    if (!str || typeof str !== 'string') return false;
+    str = str.replace(/[^a-zA-Z]/g, '').toLowerCase();
+    end = str.length - 1;
+  }
+  if (start >= end) return true;
+  if (str[start] !== str[end]) return false;
+  return palindrome(str, ++start, --end);
+}
