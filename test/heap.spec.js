78843e3337d27efc3f195abfc379f8b219e07ea6
William Barnes <barnesw91291@gmail.com>
2017-01-29 13:06:54 -0800

    added basic test script for Heap

---
+++ test/heap.spec.js
@@ -0,0 +1,9 @@
+const expect = require('expect.js');
+const Heap = require('../algos/heap.js');
+
+
+describe('Heap Datastructure', () => {
+  it('should be a function', () => {
+    expect(Heap).to.be.a('function');
+  });
+});
