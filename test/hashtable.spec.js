9b612729fb7a5fb2d2145a9a2f7440f92c4dd61a
William Barnes <barnesw91291@gmail.com>
2017-01-29 13:06:24 -0800

    added basic test script for HashTable

---
+++ test/hashtable.spec.js
@@ -0,0 +1,9 @@
+const expect = require('expect.js');
+const HashTable = require('../algos/hashtable.js');
+
+
+describe('HashTable Datastructure', () => {
+  it('should be a function', () => {
+    expect(HashTable).to.be.a('function');
+  });
+});
