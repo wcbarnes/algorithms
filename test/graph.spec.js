2b81c939843c7eaa9ecdf01d7a0ff1e421153548
William Barnes <barnesw91291@gmail.com>
2017-01-29 13:05:37 -0800

    added basic test script for graph

---
+++ test/graph.spec.js
@@ -0,0 +1,9 @@
+const expect = require('expect.js');
+const graph = require('../algos/graph.js');
+
+
+describe('Graph Datastructure', () => {
+  it('should be a function', () => {
+    expect(graph).to.be.a('function');
+  });
+});
