7ce1b691d3a7e826ec7c17a29f75df36207585e8
William Barnes <barnesw91291@gmail.com>
2017-01-30 17:41:02 -0800

    Created stack challenge

---
+++ algos/stack.js
@@ -0,0 +1,25 @@
+/*
+ * Create a Stack with the following methods
+ * a. push
+ * b. pop
+ * c. forEach
+ */
+
+function Stack() {
+  this.store = [];
+  this.length = 0;
+}
+
+Stack.prototype.push = function push(value) {
+
+};
+
+Stack.prototype.pop = function pop() {
+
+};
+
+Stack.prototype.forEach = function forEach() {
+
+};
+
+module.exports = Stack;
