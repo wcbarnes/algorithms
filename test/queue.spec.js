b84ef0c4c670f01dde0f64e87d2ae36f3b16f4d8
William Barnes <barnesw91291@gmail.com>
2017-01-30 16:39:48 -0800

    Queue test file

---
+++ test/queue.spec.js
@@ -0,0 +1,9 @@
+const expect = require('expect.js');
+const Queue = require('../algos/queue.js');
+
+
+describe('Queue Datastructure', () => {
+  it('should be a function', () => {
+    expect(Queue).to.be.a('function');
+  });
+});
