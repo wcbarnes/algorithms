c1dbb511dec9f706e8e7eb746b17245ac6af18ba
William Barnes <barnesw91291@gmail.com>
2017-01-30 17:00:43 -0800

    trie basic test

---
+++ test/trie.spec.js
@@ -0,0 +1,9 @@
+const expect = require('expect.js');
+const Trie = require('../algos/trie.js');
+
+
+describe('Trie Datastructure', () => {
+  it('should be a function', () => {
+    expect(Trie).to.be.a('function');
+  });
+});
