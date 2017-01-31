f902f5d7adde9bf54382c2fca0c969d2e1951a3c
William Barnes <barnesw91291@gmail.com>
2017-01-30 16:59:45 -0800

    stack basic test

---
+++ test/stack.spec.js
@@ -0,0 +1,9 @@
+const expect = require('expect.js');
+const Stack = require('../algos/stack.js');
+
+
+describe('Stack Datastructure', () => {
+  it('should be a function', () => {
+    expect(Stack).to.be.a('function');
+  });
+});
