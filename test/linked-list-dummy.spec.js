2b97db9a10f33ab8d52c0c1868069ff8adb71483
William Barnes <barnesw91291@gmail.com>
2017-01-30 16:38:02 -0800

    formatting test files

---
+++ test/linked-list-dummy.spec.js
@@ -0,0 +1,9 @@
+const expect = require('expect.js');
+const LinkedList = require('../algos/linked-list-dummy.js');
+
+
+describe('Linked List Dummy Datastructure', () => {
+  it('should be a function', () => {
+    expect(LinkedList).to.be.a('function');
+  });
+});
---
+++ test/linked-list.spec.js
@@ -0,0 +1,9 @@
+const expect = require('expect.js');
+const LinkedList = require('../algos/linked-list.js');
+
+
+describe('Linked List Datastructure', () => {
+  it('should be a function', () => {
+    expect(LinkedList).to.be.a('function');
+  });
+});
