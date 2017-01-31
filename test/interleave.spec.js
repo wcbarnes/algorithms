652236ed6e05d2467f0e16a9a1fbd40517ce1795
William Barnes <barnesw91291@gmail.com>
2017-01-30 17:03:13 -0800

    updated interleave describe

--- test/interleave.spec.js
+++ test/interleave.spec.js
@@ -2,7 +2,7 @@ const expect = require('expect.js');
 const interleave = require('../algos/interleave.js');
 
 
-describe('interleave', () => {
+describe('interleave two arrays', () => {
   it('should be a function', () => {
     expect(interleave).to.be.a('function');
   });
