b8ab554f2773ebefaecbab4689d4d4f5e20354d1
William Barnes <barnesw91291@gmail.com>
2017-01-30 18:23:09 -0800

    trie challenges

--- algos/trie.js
+++ algos/trie.js
@@ -1,8 +1,9 @@
 /*
  * Create a Trie with the following methods
- * a. 
- * b. 
- * c. 
+ * a. insert
+ * b. contains
+ * c. remove
+ * d. findAnagrams (find all words in the trie that are anagrams and return them in an array)
  */
 
 function Trie() {
@@ -10,35 +11,19 @@ function Trie() {
   this.length = 0;
 }
 
-Trie.prototype. = function () {
+Trie.prototype.insert = function insert(string) {
 
 };
 
-Trie.prototype. = function () {
+Trie.prototype.contains = function contains(string) {
 
 };
 
-Trie.prototype. = function () {
+Trie.prototype.remove = function remove() {
 
 };
 
-Trie.prototype. = function () {
-
-};
-
-Trie.prototype. = function () {
-
-};
-
-Trie.prototype. = function () {
-
-};
-
-Trie.prototype. = function () {
-
-};
-
-Trie.prototype. = function () {
+Trie.prototype.findAnagrams = function findAnagrams() {
 
 };
 
