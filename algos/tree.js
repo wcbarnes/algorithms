4b80bf034089a900b5fbdb7f25b6b1e165defa9d
William Barnes <barnesw91291@gmail.com>
2017-01-30 17:57:14 -0800

    tree and trie base structure for algorithms

---
+++ algos/tree.js
@@ -0,0 +1,45 @@
+/*
+ * Create a Tree with the following methods
+ * a. 
+ * b. 
+ * c. 
+ */
+
+function Tree() {
+  this.store = [];
+  this.length = 0;
+}
+
+Tree.prototype. = function () {
+
+};
+
+Tree.prototype. = function () {
+
+};
+
+Tree.prototype. = function () {
+
+};
+
+Tree.prototype. = function () {
+
+};
+
+Tree.prototype. = function () {
+
+};
+
+Tree.prototype. = function () {
+
+};
+
+Tree.prototype. = function () {
+
+};
+
+Tree.prototype. = function () {
+
+};
+
+module.exports = Tree;
---
+++ algos/trie.js
@@ -0,0 +1,45 @@
+/*
+ * Create a Trie with the following methods
+ * a. 
+ * b. 
+ * c. 
+ */
+
+function Trie() {
+  this.store = [];
+  this.length = 0;
+}
+
+Trie.prototype. = function () {
+
+};
+
+Trie.prototype. = function () {
+
+};
+
+Trie.prototype. = function () {
+
+};
+
+Trie.prototype. = function () {
+
+};
+
+Trie.prototype. = function () {
+
+};
+
+Trie.prototype. = function () {
+
+};
+
+Trie.prototype. = function () {
+
+};
+
+Trie.prototype. = function () {
+
+};
+
+module.exports = Trie;
