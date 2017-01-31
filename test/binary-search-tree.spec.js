0cdf7ce7c621859fa0789368b9391c63576b9b11
William Barnes <barnesw91291@gmail.com>
2017-01-30 17:05:59 -0800

    typo fixes

--- test/binary-search-tree.spec.js
+++ test/binary-search-tree.spec.js
@@ -2,7 +2,7 @@ const expect = require('expect.js');
 const BST = require('../algos/binary-search-tree.js');
 
 
-describe('Binary Search Tree', () => {
+describe('Binary Search Tree datastructure', () => {
   it('should be a function', () => {
     expect(BST).to.be.a('function');
   });
--- test/binary-search.spec.js
+++ test/binary-search.spec.js
@@ -2,7 +2,7 @@ const expect = require('expect.js');
 const binarySearch = require('../algos/binary-search');
 
 
-describe('binarySearch', () => {
+describe('binarySearch should find an element faster the linear time', () => {
   it('should be a function', () => {
     expect(binarySearch).to.be.a('function');
   });
