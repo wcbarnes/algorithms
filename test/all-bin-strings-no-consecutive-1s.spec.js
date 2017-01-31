737c0f1db3cc3d563d89e974ecbc229bda838a09
William Barnes <barnesw91291@gmail.com>
2017-01-31 08:51:27 -0800

    first test for all binary strings up to number without consectuvei 1s

--- test/all-bin-strings-no-consecutive-1s.spec.js
+++ test/all-bin-strings-no-consecutive-1s.spec.js
@@ -6,4 +6,8 @@ describe('get no consectutive 1s binary', () => {
   it('should be a function', () => {
     expect(getOnes).to.be.a('function');
   });
+
+  it('should return an array of binary strings', () => {
+    expect(getOnes(2).sort()).to.eql(['00', '01', '10'].sort());
+  });
 });
