b801255830b14647cbf325ce296d0097a89a36c2
William Barnes <barnesw91291@gmail.com>
2017-01-30 17:00:13 -0800

    tree basic test

---
+++ test/tree.spec.js
@@ -0,0 +1,9 @@
+const expect = require('expect.js');
+const Tree = require('../algos/tree.js');
+
+
+describe('Tree Datastructure', () => {
+  it('should be a function', () => {
+    expect(Tree).to.be.a('function');
+  });
+});
