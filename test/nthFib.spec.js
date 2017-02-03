943f8745e66256230ebd2998b8cf4c9650d6c8e3
William Barnes <barnesw91291@gmail.com>
2017-02-02 19:50:26 -0800

    added basic test for nthFib

---
+++ test/nthFib.spec.js
@@ -0,0 +1,9 @@
+const expect = require('expect.js');
+const nthFib = require('../algos/nthFib.js');
+
+
+describe('nthFib should sort the array', () => {
+  it('should be a function', () => {
+    expect(nthFib).to.be.a('function');
+  });
+});
